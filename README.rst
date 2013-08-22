87ffe1a9c6d32c60dd90803ee9d2c0994c99ef5e
Claire Revillet <grenoya@zarb.org>
2013-08-22 10:27:53 +0200

    - new version to be used in Python console

--- README.rst
+++ README.rst
@@ -1,3 +1,6 @@
+OBSOLETTE
+*********
+
 L'application Tirelire
 ======================
 
--- tirelire.py
+++ tirelire.py
@@ -1,140 +1,123 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+""" Gestion simplifiée d'une tirelire """
 from __future__ import print_function
+import shelve
 from datetime import datetime
-from os.path import exists, splitext
-from os import remove
-from pickle import dump, load
-from boite import Boite
+from datetime import date as dtdate
 
 
-authorisedTransactions = {
-    "Cochon Matelas": "Cochon Depense",
-    "Cochon Matelas": "Cochon Virement",
-    "Exterieur": "Cochon Matelas",
-    "Cochon Depense": "Exterieur",
-    "Cochon Depense": "Cochon Matelas",
-    "Cochon Virement": "Cochon Matelas",
-    "Cochon Virement": "Compte Epargne",
-    "Compte Epargne": "Cochon Matelas",
-    }
+class Cochons():
+    """ Tirelire """
+    def __init__(self):
+        #: montant total possédé
+        self._courant = 0.0
+        #: liste des cochons
+        self._noms = []
+        #: montant des cochons
+        self._montants = []
+        #: montant non attribué à un cochon
+        self._cagnotte = 0.0
+        #: historique des mouvements du total possédé
+        self._historiqueExterne = []
+        #: historique des mouvements internes
+        self._historiqueInterne = []
+        #: nom du fichier de sauvegarde
+        self._chemin = None
+
+    def ajouteCourant(self, montant, commentaire, date=None):
+        """ apport d'argent """
+        date, msg = gereDate(date)
+        self._courant += montant
+        self._cagnotte += montant
+        self._historiqueExterne.append((date, montant, "versement",
+                                        commentaire))
+        self._historiqueInterne.append((date, montant, "versement",
+                                        commentaire))
+        print(msg)
+        return msg
+
+    def ajouteCochon(self, nom, date=None):
+        """ création d'un cochon """
+        date, msg = gereDate(date)
+        self._noms.append(nom)
+        self._montants.append(0.0)
+        self._historiqueInterne.append((date, None, "creation Cochon",
+                                        nom))
+        print(msg)
+        return msg
+
+    def verseCochon(self, nom, montant, date=None):
+        """ versement d'argent de la cagnotte vers un cochon """
+        date, msg = gereDate(date)
+        indice = self._noms.index(nom)
+        self._montants[indice] += montant
+        self._cagnotte -= montant
+        self._historiqueInterne.append((date, montant,
+                                        "versement Cochon", nom))
+        print(msg)
+        return msg
+
+    def depense(self, nom, montant, commentaire, date=None):
+        """ depense l'argent d'un cochon """
+        date, msg = gereDate(date)
+        indice = self._noms.index(nom)
+        self._montants[indice] -= montant
+        self._courant -= montant
+        self._historiqueExterne.append((date, montant, "depense", commentaire))
+        self._historiqueInterne.append((date, montant, "depense Cochon", nom))
+        print(msg)
+        return msg
+
+    def etatCochons(self):
+        """ affiche l'état de chaque cochon """
+        for elem in self._noms:
+            print("%s:\t%f" % (elem, self._montants[self._noms.index(elem)]))
+
+    def etatCagnotte(self):
+        """ affiche l'état de la cagnotte """
+        print("cagnotte:\t%f" % self._cagnotte)
+
+    def afficheHistorique(self):
+        """ affiche l'historique des recettes et dépenses """
+        for elem in self._historiqueExterne:
+            print("%s\t%s\t%s\t%s" % elem)
+
+    def sauve(self, nom_fichier=None):
+        """ sauve la tirelire dans un fichier """
+        if not nom_fichier:
+            self._chemin = nom_fichier
+        fic = shelve.open(self._chemin)
+        fic['courant'] = self._courant
+        fic['noms'] = self._noms
+        fic['montants'] = self._montants
+        fic['cagnotte'] = self._cagnotte
+        fic['historiqueExterne'] = self._historiqueExterne
+        fic['historiqueInterne'] = self._historiqueInterne
+        fic.close()
+
+    def charge(self, nom_fichier):
+        """ charge une tirelire depuis une sauvegarde """
+        self._chemin = nom_fichier
+        fic = shelve.open(self._chemin)
+        self._courant = fic['courant']
+        self._noms = fic['noms']
+        self._montants = fic['montants']
+        self._cagnotte = fic['cagnotte']
+        self._historiqueExterne = fic['historiqueExterne']
+        self._historiqueInterne = fic['historiqueInterne']
+        fic.close()
 
 
-class Tirelire():
-
-    @classmethod
-    def new(cls, name, value, **kwargs):
-        # Check the existance of a previus eponym Tirelire in the same directory
-        # before creating it.
-        if name.endswith('.tir'):
-            filename = name
-        else:
-            filename = name + '.tir'
-
-        if exists(filename):
-            if "force" in kwargs:
-                msg = "Tirelire %s already exists! use --force to rewrite it" \
-                        % filename
-                return -1, msg
-            remove(filename)
-
-        # TODO: verification of the value validity
-        return Tirelire(filename, value)
-
-
-    def __init__(self, name, value):
-        self.ficname = name
-        self.creationdate = datetime.now()
-        self.courant = Boite("Courant", 0, "Compte Courant", self)
-        self.exterieur = Boite("Exterieur", 0, "Exterieur", self)
-        self.matelas = Boite("Matelas", value, "Cochon Matelas", self)
-        self.boxes = []
-        self.updateCourant()
-
-    def updateCourant(self):
-        self.courant.montant = self.matelas.montant
-        for elem in self.boxes:
-            if elem.info != "Epargne":
-                self.courant.montant += elem.montant
-
-    def addCompteEpargne(self, name, value, **kwargs):
-        #TODO: check the validity of value
-        # Add a Saving account to the Tirelire.
-        for elem in self.boxes:
-            if elem.name == name:
-                if "force" in kwargs:
-                    msg = "%s account already exists ! " \
-                        + "use --force to remove it" % name
-                    return -1, msg
-                self.removeCompteEpargne(name)
-        self.boxes.append(Boite(name, value, "Compte Epargne", self))
-        return
-
-    def addCochon(self, name, pigType, **kwargs):
-        # Add a Cochon to the Tirelire.
-        for elem in self.boxes:
-            if elem.name == name:
-                if "force" in kwargs:
-                    msg = "%s pig already exists ! " \
-                        + "use --force to remove it" % name
-                    return -1, msg
-                self.removeCochon(name)
-        self.boxes.append(Boite(name, 0, pigType, self))
-        return
-
-    def removeBox(self, name, **kwargs):
-        for elem in self.boxes:
-            if elem.name == name:
-                if "force" in kwargs:
-                    msg = "Emptying %s %s before destruction" \
-                            % (elem.info, elem.name)
-                    self.transaction(elem.montant, elem, self.matelas, msg)
-                    self.boxes.remove(elem)
-                    return 1, "Box %s removed with force" % elem.name
-                if elem.montant != 0:
-                    return -2, "Box %s is not empty ! use --force" \
-                        % elem.name
-                self.boxes.remove(elem)
-                return 1, "Box %s removed the soft way" % name
-
-    def transaction(self, value, origin, destination, comment):
-        # TODO: verification of the value validity
-        if (origin.info, destination.info) in authorisedTransactions.items():
-            origin.getMoney(value, destination, comment)
-            destination.putMoney(value, origin, comment)
-
-    def save(self):
-        tirFile = open(self.ficname, 'w')
-        dump(self, tirFile)
-        tirFile.close()
-
-    def summary(self, beginDate, **kwargs):
-        #TODO: make the date optional
-        # return all the transations since the given date.
-        return ''
-
-    def __repr__(self):
-        # return the actual status of the Tirelire.
-        message = "Tirelire %s cree le %s\n" % (self.ficname, self.creationdate)
-        message += repr(self.courant)
-        message += repr(self.matelas)
-        for elem in self.boxes:
-                message += repr(elem)
-        return message
-
-
-def loadTirelire(name):
-    if splitext(name)[1] != '.tir':
-        name += '.tir'
-    if not exists(name):
-        return 1, "The Tirelire %s doesn't exist." % name
-    tirFile = open(name, 'r')
-    tir = load(tirFile)
-    tirFile.close()
-    return tir
-
-
-if __name__ == "__main__":
-    tirtest = Tirelire()
-    print(tirtest)
-
+def gereDate(date):
+    """ Renvoie la sate fournie au format datetime """
+    msg = ''
+    if not date:
+        date = dtdate.today()
+    else:
+        try:
+            date = datetime.strptime(date, "%Y-%m-%d").date()
+        except ValueError:
+            msg = "Date incomprehensible, on prend la date actuelle"
+            date = dtdate.today()
+    return date, msg
